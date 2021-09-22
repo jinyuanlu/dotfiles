58569bc7400a9b26eb5f79008e3fe303c5df115d
Jinyuan Lu <me@jinyuanlu.net>
2021-09-23 07:16:30 +0800

    feat: add multiple cursor to hydra

--- src/emacs.d/lisp/init-hydra.el
+++ src/emacs.d/lisp/init-hydra.el
@@ -49,5 +49,33 @@ Frames: _f_rame new  _df_ delete
 (global-set-key (kbd "C-c C-w") 'hydra-window/body)
 
 
+(defhydra hydra-multiple-cursors (:hint nil)
+  "
+ Prev^^             Next^^           Miscellaneous           % 2(mc/num-cursors) cursor%s(if (> (mc/num-cursors) 1) \"s\" \"\")
+------------------------------------------------------------------
+ [_p_]   Next     [_n_]   Next     [_l_] Edit lines  [_0_] Insert numbers
+ [_P_]   Skip     [_N_]   Skip     [_a_] Mark all    [_A_] Insert letters
+ [_M-p_] Unmark   [_M-n_] Unmark   [_s_] Search      [_q_] Quit
+ [_|_] Align with input CHAR       [Click] Cursor at point"
+  ("l" mc/edit-lines)
+  ("a" mc/mark-all-like-this :exit t)
+  ("n" mc/mark-next-like-this)
+  ("N" mc/skip-to-next-like-this)
+  ("M-n" mc/unmark-next-like-this)
+  ("p" mc/mark-previous-like-this)
+  ("P" mc/skip-to-previous-like-this)
+  ("M-p" mc/unmark-previous-like-this)
+  ("|" mc/vertical-align)
+  ("s" mc/mark-all-in-region-regexp :exit t)
+  ("0" mc/insert-numbers :exit t)
+  ("A" mc/insert-letters :exit t)
+  ("<mouse-1>" mc/add-cursor-on-click)
+  ;; Help with click recognition in this hydra
+  ("<down-mouse-1>" ignore)
+  ("<drag-mouse-1>" ignore)
+  ("q" nil))
+
+(global-set-key (kbd "C-c e") 'hydra-multiple-cursors/body)
+
 (provide 'init-hydra)
 ;;; init-hydra.el ends here
