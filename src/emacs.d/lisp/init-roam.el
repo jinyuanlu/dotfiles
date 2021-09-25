4262d11478516c35a10965f075da35ed677af019
Jinyuan Lu <me@jinyuanlu.net>
2021-09-25 13:46:23 +0800

    feat: v2 ack & dir

--- src/emacs.d/lisp/init-roam.el
+++ src/emacs.d/lisp/init-roam.el
@@ -4,9 +4,9 @@
 
 (require-package 'org-roam)
 
-(setq org-roam-directory "~/.org/wiki/")
+(setq org-roam-directory (file-truename "~/Private/org/wiki/"))
 
-(setq org-roam-buffer-position 'bottom)
+(setq org-roam-v2-ack t)
 
 (provide 'init-roam)
 ;;; init-roam.el ends here
