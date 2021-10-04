14d65a583e768a65fc812875d4b4469fa2a6199f
Jinyuan Lu <me@jinyuanlu.net>
2021-10-04 14:53:04 +0800

    feat: add scala support

--- src/emacs.d/lisp/init-scala.el
+++ src/emacs.d/lisp/init-scala.el
@@ -4,7 +4,20 @@
 
 ;; Enable scala-mode for highlighting, indentation and motion commands
 (when (maybe-require-package 'scala-mode)
-  (maybe-require-package 'sbt-mode))
+  (require-package 'sbt-mode)
+  (require-package 'lsp-metals)
+  (require-package 'lsp-mode)
+  (require-package 'lsp-ui)
+  )
+
+(with-eval-after-load 'scala-mode
+  (add-hook 'scala-mode-hook 'lsp)
+  )
+
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook 'lsp-lens-mode)
+  )
+
 
 
 (provide 'init-scala)
