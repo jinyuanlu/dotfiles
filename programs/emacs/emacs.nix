1c7ba3a0a5a1aeaa5f2e14ae5af7d8033df0b0c1
Jinyuan Lu <me@jinyuanlu.net>
2022-04-17 10:28:28 +0800

    feat: modify emacs config

---
+++ programs/emacs/emacs.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }:
+
+let
+  sources = import ../../nix/sources.nix;
+in {
+  programs.emacs = {
+    enable = true;
+};
+
+  home.file = {
+    ".emacs.d" = {
+      source = ../../src/emacs/.emacs.d;
+      recursive = true;
+    };
+  };
+}
