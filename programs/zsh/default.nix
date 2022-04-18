11792b09fe3e23a0e85fe9caa4ed1271cce29fbf
Jinyuan Lu <me@jinyuanlu.net>
2022-04-18 08:23:18 +0800

    style: format

--- programs/zsh/default.nix
+++ programs/zsh/default.nix
@@ -1,6 +1,5 @@
-{pkgs, ...}:
-{
-  home.packages = [ pkgs.direnv pkgs.fzf pkgs.starship  pkgs.zoxide];
+{ pkgs, ... }: {
+  home.packages = [ pkgs.direnv pkgs.fzf pkgs.starship pkgs.zoxide ];
 
   programs.zsh = {
     enable = true;
@@ -13,12 +12,14 @@
     };
 
     shellAliases = {
-      setproxy="export ALL_PROXY=socks5://127.0.0.1:1080";
-      unsetproxy="unset ALL_PROXY";
-      bal="ledger -f ~/.fin/fin.dat balance";
-      ee="emacsclient";
-      eth="curl 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,CNY' | python -m json.tool";
-      btc="curl 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=BTC,USD,CNY' | python -m json.tool";
+      setproxy = "export ALL_PROXY=socks5://127.0.0.1:1080";
+      unsetproxy = "unset ALL_PROXY";
+      bal = "ledger -f ~/.fin/fin.dat balance";
+      ee = "emacsclient";
+      eth =
+        "curl 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,CNY' | python -m json.tool";
+      btc =
+        "curl 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=BTC,USD,CNY' | python -m json.tool";
     };
 
     initExtra = builtins.readFile ./zshrc;
