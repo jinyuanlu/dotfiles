30d137706ccc3512f17524ca1c8d6bc333a1bc55
Jinyuan Lu <lu.j@ctw.inc>
2022-07-24 17:36:09 +0900

    feat: add tree

--- home.nix
+++ home.nix
@@ -7,6 +7,7 @@
   home.homeDirectory = "/Users/lujinyuan";
 
   home.packages = with pkgs; [
+    tree
     ripgrep
     stack
     ledger
@@ -23,7 +24,6 @@
     jdk8
     coursier
     # -- For: metals-emacs
-
     coq
     bazel_5
     terraform
