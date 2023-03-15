431bd49c93f161e6d5570d977979f3bf13c1c2b9
Jinyuan Lu <me@jinyuanlu.net>
2023-03-15 22:07:39 +0900

    feat: alias

--- programs/zsh/default.nix
+++ programs/zsh/default.nix
@@ -24,6 +24,7 @@
         "curl 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=BTC,USD,CNY' | python -m json.tool";
       # get finder current directory
       fcd = ''osascript -e "tell app \"Finder\" to POSIX path of (insertion location as alias)"'';
+      ts="echo -e \"\$(TZ=\"America/New_York\" date) - New York\n\$(TZ=\"Europe/London\" date) - London\n\$(TZ=\"Asia/Tokyo\" date) - Tokyo\n\$(TZ=\"Asia/Shanghai\" date) - Beijing\"";
     };
 
     initExtra = builtins.readFile ./zshrc;
