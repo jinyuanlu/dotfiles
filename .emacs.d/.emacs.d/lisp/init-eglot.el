c201dc4e1aa6000faa98cf6d0f05d34389100605
Luke <app@luke.archi>
2025-02-09 12:20:58 +0900

    emacs

---
+++ .emacs.d/.emacs.d/lisp/init-eglot.el
@@ -0,0 +1,20 @@
+;;; init-eglot.el --- LSP support via eglot          -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+;; For Emacs >= 27
+(setq read-process-output-max (* 1024 1024))
+
+
+(when (maybe-require-package 'eglot)
+  (maybe-require-package 'consult-eglot))
+
+(with-eval-after-load "eglot"
+  (add-to-list 'eglot-server-programs '(python-mode . ("pyright")))
+  (add-to-list 'eglot-server-programs '((c++-mode c-mode) "ccls"))
+  )
+
+(provide 'init-eglot)
+;;; init-eglot.el ends here
