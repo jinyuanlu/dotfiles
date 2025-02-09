223d6b9d6753360b99fc722684852bb06efcb111
Luke <app@luke.archi>
2025-02-09 16:25:54 +0900

    update

--- .emacs.d/.emacs.d/lisp/init-cmake.el
+++
@@ -1,11 +0,0 @@
-;;; init-cmake.el --- Support for working with CMAKE -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
-
-;; Enable cmake-mode for highlighting, indentation and motion commands
-(require-package 'cmake-mode)
-
-
-
-(provide 'init-cmake)
-;;; init-cmake.el ends here
--- .emacs.d/.emacs.d/lisp/init-yasnippet.el
+++
@@ -1,5 +0,0 @@
-(require-package 'yasnippet)
-(add-to-list 'load-path
-             "~/.emacs.d/plugins/yasnippet")
-
-(provide 'init-yasnippet)
