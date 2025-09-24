810d38ce90a1aa21688ce86340841f2ef8214dde
Luke <t@luke.archi>
2025-09-24 15:47:40 +0900

    fix(emacs): use correct TLA+ package name
    Replace non-existent 'tla-mode' with 'tla-pcal-mode' which is the
    actual TLA+ mode package available in MELPA. This fixes the
    initialization error preventing Emacs from loading properly.

--- .emacs.d/lisp/init-tla.el
+++ .emacs.d/lisp/init-tla.el
@@ -1,8 +1,8 @@
 ;;; init-tla.el --- Support for working with TLA+ -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
-(require-package 'tla-mode)
-(add-to-list 'auto-mode-alist '("\\.tla\\'" . tla-mode))
+(require-package 'tla-pcal-mode)
+(add-to-list 'auto-mode-alist '("\\.tla\\'" . tla-pcal-mode))
 
 (provide 'init-tla)
 ;;; init-tla.el ends here
